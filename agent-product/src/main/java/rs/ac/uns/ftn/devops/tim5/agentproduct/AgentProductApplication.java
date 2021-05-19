c0f245356bc978713457a4dda1e8af8a1692208e
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-05-19 14:34:53 +0200

    init agent-product service #16

---
+++ agent-product/src/main/java/rs/ac/uns/ftn/devops/tim5/agentproduct/AgentProductApplication.java
@@ -0,0 +1,13 @@
+package rs.ac.uns.ftn.devops.tim5.agentproduct;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgentProductApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AgentProductApplication.class, args);
+    }
+
+}
