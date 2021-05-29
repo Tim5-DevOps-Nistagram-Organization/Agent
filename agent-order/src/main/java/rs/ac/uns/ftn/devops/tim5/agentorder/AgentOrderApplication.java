5d02d9fb21f3187b197452b0ded4ed5eed61db3e
DusanBucan <34024330+DusanBucan@users.noreply.github.com>
2021-05-29 19:44:53 +0200

    Infrastructure/consul spring cloud gateway (#22)
    Add consul, spring cloud, set up dockerfile and docker compose for dev, fix docker-compose for test

--- agent-order/src/main/java/rs/ac/uns/ftn/devops/tim5/agentorder/AgentOrderApplication.java
+++ agent-order/src/main/java/rs/ac/uns/ftn/devops/tim5/agentorder/AgentOrderApplication.java
@@ -2,9 +2,19 @@ package rs.ac.uns.ftn.devops.tim5.agentorder;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableFeignClients
+@RestController
 public class AgentOrderApplication {
+
+    @RequestMapping("/health")
+    public String home() {
+        return "Hello world";
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(AgentOrderApplication.class, args);
