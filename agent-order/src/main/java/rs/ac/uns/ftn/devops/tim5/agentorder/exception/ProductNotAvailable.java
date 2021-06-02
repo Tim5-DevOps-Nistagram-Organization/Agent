76991010720c19faaf9f60e856b2da85d44e4ac9
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-02 16:01:54 +0200

    pom.xml and exception

--- agent-order/src/main/java/rs/ac/uns/ftn/devops/tim5/agentorder/exception/ProductNotAvailable.java
+++ agent-order/src/main/java/rs/ac/uns/ftn/devops/tim5/agentorder/exception/ProductNotAvailable.java
@@ -1,6 +1,6 @@
 package rs.ac.uns.ftn.devops.tim5.agentorder.exception;
 
-public class ProductNotAvailable extends Throwable {
+public class ProductNotAvailable extends Exception {
     public ProductNotAvailable(String name) {
         super(String.format("Product %s is not available.", name));
     }
