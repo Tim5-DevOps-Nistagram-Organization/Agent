d7cec614dc67d4394f74d7574ee8f2532bd9d5ed
MicaTravica <40805992+MicaTravica@users.noreply.github.com>
2021-06-03 18:57:12 +0200

    test for product #7

---
+++ agent-product/src/test/java/rs/ac/uns/ftn/devops/tim5/agentproduct/constants/ImageConstants.java
@@ -0,0 +1,7 @@
+package rs.ac.uns.ftn.devops.tim5.agentproduct.constants;
+
+public class ImageConstants {
+
+    public static String BASE_URL = "/image";
+    public static Long IMAGE_ID_DOESNT_EXIST = 2L;
+}
