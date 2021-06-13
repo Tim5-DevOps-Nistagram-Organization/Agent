a84dbfd7a9bdc7f7f298b014c85c4c82529a9968
Dusan Bucan <dusanbzr@gmail.com>
2021-06-13 10:48:32 +0200

    metric for unique visitor

---
+++ gateway/src/main/java/rs/ac/uns/ftn/gateway/gateway/config/HerokuPostgresConfiguration.java
@@ -0,0 +1,42 @@
+package rs.ac.uns.ftn.gateway.gateway.config;
+
+import com.zaxxer.hikari.HikariConfig;
+import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnProperty;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import javax.sql.DataSource;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+@Configuration
+@ConditionalOnProperty(
+        value = "STAGE",
+        havingValue = "PROD"
+)
+public class HerokuPostgresConfiguration {
+
+    @Value("${DATABASE_URL}")
+    private String databaseUrl;
+
+    @Bean
+    public DataSource dataSource() throws URISyntaxException {
+        URI dbUri = new URI(databaseUrl);
+
+        String username = dbUri.getUserInfo().split(":")[0];
+        String password = dbUri.getUserInfo().split(":")[1];
+        String dbUrl = "jdbc:postgresql://" + dbUri.getHost() + ':' + dbUri.getPort() + dbUri.getPath();
+
+        HikariConfig config = new HikariConfig();
+        config.setJdbcUrl(dbUrl);
+        config.setUsername(username);
+        config.setPassword(password);
+        // because Heroku postgres in free tier accepts 20 connection in total
+        config.setMaximumPoolSize(2);
+
+        HikariDataSource ds = new HikariDataSource(config);
+        return ds;
+    }
+}
